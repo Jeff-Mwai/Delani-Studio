8b3108d878647997c025a36e5205f07914ed5cba
Jeff-Mwai <jeffmwai3@gmail.com>
2020-08-31 00:42:21 +0300

    Added ready and toggle functions

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $('#black-studio').hover(function(){
+    $('.text1').toggle();
+    })
+  $('#ontario').hover(function(){
+    $('.text2').toggle();
+    })
+ $('#pyramids').hover(function(){
+    $('.text3').toggle();
+    })
+$('#jim-carry').hover(function(){
+    $('.text4').toggle();
+    })
+$('#nice-day').hover(function(){
+    $('.text5').toggle();
+    })
+
+
+
+
+
+
+
+
+
+
+
+$('#design').click(function() {
+$('#click-text').toggle();
+})
+
+});
